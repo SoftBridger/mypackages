b5c0fba1f7aab41d3fa8e801e6605d6bb9e8ffa8
yzg <yzg915@163.com>
2024-11-30 11:27:19 +0800

    nothing

---
+++ myvals.py
@@ -0,0 +1,16 @@
+"""
+用于存放常用参数
+"""
+con_dict_huawei = {"host": '60.204.212.22',
+                   "port": 3306,
+                   "user": "root",
+                   "passwd": "WTgc_2022",
+                   "db": "clause",
+                   "charset": 'utf8mb4'}
+
+con_dict_local = {"host": '127.0.0.1',
+                  "port": 3306,
+                  "user": "root",
+                  "passwd": "Py15_Js23",
+                  "db": "test",
+                  "charset": 'utf8mb4'}
